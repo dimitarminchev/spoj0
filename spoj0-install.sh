ee4d49dc9643568555507591aeb25fa317ad7330
Dimitar Minchev <dimitar.minchev@gmail.com>
2016-12-22 12:45:46 +0200

    INSTALL

--- spoj0-install.sh
+++ spoj0-install.sh
@@ -1,18 +1,27 @@
-#!/bin/bash -verbose
+#!/bin/bash
 # Note: This Script should be run as root!
 
 # Install packages
-apt-get install launchtool libapache2-mod-perl2 apache2 mysql-server mysql-client
+echo "INSTALL PACKAGES..."
+apt-get install launchtool libapache2-mod-perl2 apache2 mysql-server mysql-client libdbi-perl libdbd-mysql-perl
+echo "Enter to continue..."
+read
 
 # Create users
+echo "CREATE USERS..."
 useradd --create-home --password spoj0 spoj0
 useradd --create-home --password spoj0run spoj0run
+echo "Enter to continue..."
+read
 
 # Get from repository
+echo "GET FROM REPO..."
 SPOJ_GIT_REPO=https://github.com/dimitarminchev/spoj0.git
 git clone $SPOJ_GIT_REPO
 cp -r spoj0/ /home/
 # rm -r spoj0/
+echo "Enter to continue..."
+read
 
 # Back home
 cd /home/spoj0
@@ -23,10 +32,13 @@ chmod 755 *.pl
 chmod 755 *.sh
 
 # MySQL
-echo "Enter mysql password:"
+echo "MYSQL..."
 mysql -p < spoj0.sql
+echo "Enter to continue..."
+read
 
 # Apache
+echo "APACHE..."
 cat <<EOT > /etc/apache2/sites-available/spoj0.conf
 Alias /spoj /home/spoj0/web2
 <Directory /home/spoj0/web2>
@@ -37,11 +49,14 @@ allow from all
 </Directory>
 EOT
 a2ensite spoj0.conf
-service apache2 restart
+service apache2 fource-reload
+echo "Enter to continue..."
+read
+
 
 # Run
-echo "Run..."
-./spoj0-control.pl start
+# echo "Run..."
+#./spoj0-control.pl start
 
 # Import Sets
 # TODO
