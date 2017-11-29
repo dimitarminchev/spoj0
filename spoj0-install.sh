2aa04f0b027fc45c4c7bdcd50b1296222bddd686
Dimitar Minchev <dimitar.minchev@gmail.com>
2017-11-29 15:36:30 +0200

    Update spoj0-install.sh

--- spoj0-install.sh
+++ spoj0-install.sh
@@ -1,15 +1,34 @@
-#!/bin/bash -verbose
-# Note: This Script should be run as root!
+#!/bin/bash 
+
+# COLORS
+nocol="\e[0m"
+green="\e[1;32m"	
+yellow="\e[1;33m"	
+white="\e[1;37m"	
+red="\e[1;31m"	
+
+# TERMINAL BREAK (CTRL+C)
+trap 'printf "\n${red}INSTALL TERMINATED!${nocol}\n"; exit 1' 2
+
+# QUESTION
+printf "${yellow}DO YOU WANT TO START? (yes/no) ${nocol}"
+read answer
+if [ $answer = "no" ]; then
+   exit 1 # EXIT
+fi
 
 # INSTALL PACKAGES
+printf "${white}INSTALL PACKAGES ...\n${nocol}"
 apt-get update
 apt-get install php php-mcrypt php-mysql apache2 libapache2-mod-php libapache2-mod-perl2 mysql-server mysql-client libdbi-perl libdbd-mysql-perl launchtool default-jdk g++ mono-mcs --fix-missing
 
 # CREATE USERS
+printf "${white}CREATE USERS ...\n${nocol}"
 useradd --create-home --password spoj0 spoj0
 useradd --create-home --password spoj0run spoj0run
 
 # GET FROM REPO
+printf "${white}GET FROM REPO ...\n${nocol}"
 SPOJ_GIT_REPO=https://github.com/dimitarminchev/spoj0.git
 git clone $SPOJ_GIT_REPO
 cp -r spoj0/ /home/
@@ -22,10 +41,12 @@ chmod 755 *.pl
 chmod 755 *.sh
 
 # MYSQL
-echo -n "MYSQL ROOT USER PASSWORD: "
+printf "${white}MYSQL ...\n${nocol}"
+printf "${yellow}ENTER MYSQL ROOT USER PASSWORD:  ${nocol}"
 mysql -u root -p < spoj0.sql
 
 # APACHE
+printf "${white}APACHE ...\n${nocol}"
 cat <<EOT > /etc/apache2/sites-available/spoj0.conf
 Alias /spoj /home/spoj0/web
 <Directory /home/spoj0/web>
@@ -38,7 +59,8 @@ EOT
 a2ensite spoj0.conf
 service apache2 reload
 
-# SPOJ SELF TEST
+# SPOJ 
+printf "${white}SPOJ ...\n${nocol}"
 now=$(date +"%Y-%m-%d %H:%M:00")
 cat << EOT  > /home/spoj0/sets/test/set-info.conf
 name=TEST2
@@ -67,4 +89,4 @@ EOT
 ./spoj0-control.pl submit 1 2 sets/test.samples/hello_ok.cs cs hello_ok.cs
 
 # DONE
-echo "ENJOY SPOJ!"
+printf "${green}ENJOY SPOJ!${nocol}\n"
