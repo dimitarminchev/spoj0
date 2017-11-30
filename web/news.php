d55399c100cae49440f00ebf76d7cfdba970df5e
Dimitar Minchev <dimitar.minchev@gmail.com>
2017-11-30 07:28:06 +0200

    Update news.php

--- web/news.php
+++ web/news.php
@@ -6,40 +6,23 @@ $page = "news";
 include("header.php");
 
 // container
-$text <<<EOT
-<!-- container -->
-<div class="container">
-<h1>%s</h1>
-EOT;
-echo sprintf( $text, $lang["nav"]["news"] );
+echo sprintf( "<div class='container'><h1>%s</h1>", $lang["nav"]["news"] );
 
-
-// sql
-$sql = "SELECT * FROM news ORDER BY new_id DESC";
-$result = $conn->query($sql);
+// query
+$result = $conn->query("SELECT * FROM news ORDER BY new_id DESC");
 
 // execute
 if ($result->num_rows > 0)
 
 // print
-while($row = $result->fetch_assoc()) {
-$text = <<< EOT
-<h3>%s</h3>
-<div>%s</div>
-<p>%s</p>
-<hr>
-EOT;
-echo sprintf( $text, $row["topic"], (new DateTime($row["new_time"]))->format("d.m.Y H:i:s"), $row["content"]);
-}
+while($row = $result->fetch_assoc()) 
+echo sprintf( "<h3>%s</h3><div>%s</div><p>%s</p><hr>", $row["topic"], (new DateTime($row["new_time"]))->format("d.m.Y H:i:s"), $row["content"]);
 
 // close
 $conn->close();
 
 // container end
-echo <<<EOT
-</div>
-<!-- /container -->
-EOT;
+echo "</div>";
 
 // Заключителна част на документа
 include("footer.php");
