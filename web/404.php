7fa0ccd423050413bfbe5eaa897ac15e67b40e9c
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-05-16 11:30:11 +0300

    Page 404

---
+++ web/404.php
@@ -0,0 +1,19 @@
+<?php
+// current page
+$page = "contests";
+
+// header
+include("header.php");
+
+// container
+echo <<<EOT
+<div class="container">
+<div class='jumbotron alert-danger'>
+<h1>404</h1>
+<p>PAGE NOT FOUND<p>
+</div>
+</div>
+EOT;
+
+// footer
+include("footer.php");
